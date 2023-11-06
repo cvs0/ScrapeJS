fe115b140cffa834d7185943e5187736ef7380b3
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-11-06 09:07:07 -0700

    Fixes / more cookie functions.

--- src/scrapejs/Cookies.js
+++ src/scrapejs/Cookies.js
@@ -1,33 +1,69 @@
 class WebScraperCookies {
-    async getCookies(url) {
-      const page = await this.browser.newPage();
-      await page.goto(url);
-      const cookies = await page.cookies();
-      await page.close();
-      return cookies;
-    }
+  async getCookies(url) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    const cookies = await page.cookies();
+    await page.close();
+    return cookies;
+  }
 
-    async deleteCookie(url, cookieName) {
-      const page = await this.browser.newPage();
-      await page.goto(url);
-      await page.deleteCookie({ name: cookieName });
-      await page.close();
-    }
+  async deleteCookie(url, cookieName) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.deleteCookie({ name: cookieName });
+    await page.close();
+  }
 
-    async setCookie(url, cookie) {
-      const page = await this.browser.newPage();
-      await page.goto(url);
-      await page.setCookie(cookie);
-      await page.close();
-    }
-  
-    async clearCookies(url) {
-      const page = await this.browser.newPage();
-      await page.goto(url);
-      await page.deleteCookie();
-      await page.close();
-    }
+  async setCookie(url, cookie) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.setCookie(cookie);
+    await page.close();
   }
   
-  module.exports = WebScraperCookies;
+  async clearCookies(url) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.deleteCookie();
+    await page.close();
+  }
+
+  async editCookie(url, cookie) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.setCookie(cookie);
+    await page.close();
+  }
+
+  async getCookiesForDomain(url, domain) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    const cookies = (await page.cookies()).filter((cookie) => cookie.domain === domain);
+    await page.close();
+    return cookies;
+  }
+
+  async getCookieValue(url, name) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    const cookies = await page.cookies();
+    const cookie = cookies.find((c) => c.name === name);
+    const value = cookie ? cookie.value : null;
+    await page.close();
+    return value;
+  }
+
+  async clearCookiesForDomain(url, domain) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    const cookies = await page.cookies();
+    const cookiesToClear = cookies.filter((cookie) => cookie.domain === domain);
+    for (const cookie of cookiesToClear) {
+      await page.deleteCookie({ name: cookie.name, domain: cookie.domain });
+    }
+    await page.close();
+  }
+}
+  
+module.exports = WebScraperCookies;
   
