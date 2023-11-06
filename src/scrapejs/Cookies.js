c3bea07d221e903070b17eb7ca80ccb6d8c78d09
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-11-06 08:54:29 -0700

    More cookie functions

--- src/scrapejs/Cookies.js
+++ src/scrapejs/Cookies.js
@@ -1,4 +1,19 @@
 class WebScraperCookies {
+    async getCookies(url) {
+      const page = await this.browser.newPage();
+      await page.goto(url);
+      const cookies = await page.cookies();
+      await page.close();
+      return cookies;
+    }
+
+    async deleteCookie(url, cookieName) {
+      const page = await this.browser.newPage();
+      await page.goto(url);
+      await page.deleteCookie({ name: cookieName });
+      await page.close();
+    }
+
     async setCookie(url, cookie) {
       const page = await this.browser.newPage();
       await page.goto(url);
