a7b39c11fb4fe9a794e8b7c3531c0cc1051f874d
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-11-06 09:05:12 -0700

    More WebScraper functions.

--- src/scrapejs/WebScraper.js
+++ src/scrapejs/WebScraper.js
@@ -14,6 +14,51 @@ class WebScraper {
       await this.browser.close();
     }
   }
+
+  async getCurrentPage() {
+    if (this.browser) {
+      const pages = await this.browser.pages();
+      return pages[pages.length - 1];
+    }
+    return null;
+  }
+
+  async blockRequests(patterns) {
+    const page = await this.getCurrentPage();
+    if (page) {
+      await page.setRequestInterception(true);
+      page.on('request', (request) => {
+        const url = request.url();
+        if (patterns.some((pattern) => url.includes(pattern))) {
+          request.abort();
+        } else {
+          request.continue();
+        }
+      });
+    }
+  }
+
+  async unblockRequests() {
+    const page = await this.getCurrentPage();
+    if (page) {
+      await page.setRequestInterception(false);
+    }
+  }
+
+  async setUserAgent(userAgent) {
+    const page = await this.getCurrentPage();
+    if (page) {
+      await page.setUserAgent(userAgent);
+    }
+  }
+
+  async setProxy(proxyUrl) {
+    if (this.browser) {
+      await puppeteerExtra.use(PuppeteerExtraPluginProxy({
+        address: proxyUrl,
+      }));
+    }
+  }
 }
 
 module.exports = WebScraper;
