edf3ed7572e1d9ec97ff0c330ecdc3858c88fa57
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-11-06 08:59:41 -0700

    Added more functions

--- src/scrapejs/Interactions.js
+++ src/scrapejs/Interactions.js
@@ -1,25 +1,69 @@
 class WebScraperInteractions {
-    async hoverOverElement(url, selector) {
-      const page = await this.browser.newPage();
-      await page.goto(url);
-      await page.hover(selector);
-      await page.close();
-    }
-  
-    async scrollToElement(url, selector) {
-      const page = await this.browser.newPage();
-      await page.goto(url);
-      await page.$eval(selector, (element) => element.scrollIntoView());
-      await page.close();
-    }
-  
-    async emulateDevice(url, device) {
-      const page = await this.browser.newPage();
-      await page.emulate(puppeteer.devices[device]);
-      await page.goto(url);
-      await page.close();
-    }
+  async hoverOverElement(url, selector) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.hover(selector);
+    await page.close();
   }
   
-  module.exports = WebScraperInteractions;
+  async scrollToElement(url, selector) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.$eval(selector, (element) => element.scrollIntoView());
+    await page.close();
+  }
+  
+  async emulateDevice(url, device) {
+    const page = await this.browser.newPage();
+    await page.emulate(puppeteer.devices[device]);
+    await page.goto(url);
+    await page.close();
+  }
+
+  async clickAndHold(url, selector, duration) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector(selector);
+    await page.click(selector, { button: 'left', delay: duration });
+    await page.close();
+  }
+
+  async dragAndDrop(url, sourceSelector, targetSelector) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector(sourceSelector);
+    await page.waitForSelector(targetSelector);
+    await page.mouse.move(sourceSelector);
+    await page.mouse.down();
+    await page.mouse.move(targetSelector);
+    await page.mouse.up();
+    await page.close();
+  }
+
+  async typeInInput(url, selector, text) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.click(selector);
+    await page.keyboard.type(text);
+    await page.close();
+  }
+
+  async pressKey(url, key) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.keyboard.press(key);
+    await page.close();
+  }
+
+  async scrollPage(url, scrollAmount) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.evaluate((scrollAmount) => {
+      window.scrollBy(0, scrollAmount);
+    }, scrollAmount);
+    await page.close();
+  }
+}
+  
+module.exports = WebScraperInteractions;
   
--- src/scrapejs/Navigation.js
+++ src/scrapejs/Navigation.js
@@ -1,22 +1,74 @@
 class WebScraperNavigation {
-    async navigateTo(url) {
-      const page = await this.browser.newPage();
-      await page.goto(url);
-      await page.close();
-    }
-  
-    async goBack() {
-      const page = await this.browser.newPage();
-      await page.goBack();
-      await page.close();
-    }
-  
-    async goForward() {
-      const page = await this.browser.newPage();
-      await page.goForward();
-      await page.close();
-    }
+  async navigateTo(url) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.close();
   }
   
-  module.exports = WebScraperNavigation;
+  async goBack() {
+    const page = await this.browser.newPage();
+    await page.goBack();
+    await page.close();
+  }
   
+  async goForward() {
+    const page = await this.browser.newPage();
+    await page.goForward();
+    await page.close();
+  }
+
+  async reloadPage(url) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.reload();
+    await page.close();
+  }
+
+  async waitForTimeout(url, milliseconds) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.waitForTimeout(milliseconds);
+    await page.close();
+  }
+
+  async waitForNavigationTo(url) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.waitForNavigation({ url });
+    await page.close();
+  }
+
+  async waitForNetworkIdle(url, timeout, maxRequests) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.waitForFunction(
+      (timeout, maxRequests) => {
+        return (
+          window.performance.now() > timeout &&
+          window.performance.getEntriesByType('resource').length <= maxRequests
+        );
+      },
+      {},
+      timeout,
+      maxRequests
+    );
+    await page.close();
+  }
+
+  async openNewPage(url) {
+    const newPage = await this.browser.newPage();
+    await newPage.goto(url);
+    return newPage;
+  }
+
+  async waitForSelectorAndNavigate(url, selector) {
+    const page = await this.browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector(selector);
+    await page.click(selector);
+    await page.waitForNavigation();
+    await page.close();
+  }
+}
+  
+module.exports = WebScraperNavigation;
